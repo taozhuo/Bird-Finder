926483264b78089d623e3d7ddddd016df691790a
Zhuo Tao <taozhuo@zhuos-imac.gateway.2wire.net>
2014-02-23 13:09:20 -0600

    switch between table view and map view

---
+++ ZTBird/BirdResultCell.h
@@ -0,0 +1,16 @@
+//
+//  BirdResultCell.h
+//  ZTBird
+//
+//  Created by Zhuo Tao on 2/22/14.
+//  Copyright (c) 2014 Zhuo Tao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface BirdResultCell : UITableViewCell
+
+@property (nonatomic, weak) IBOutlet UILabel *nameLabel;
+@property (nonatomic, weak) IBOutlet UILabel *locationLabel;
+
+@end
--- ZTBird/NearbyViewController.h
+++ ZTBird/NearbyViewController.h
@@ -1,5 +1,5 @@
 //
-//  ViewController.h
+//  NearbyViewController.h
 //  ZTBird
 //
 //  Created by Zhuo Tao on 2/22/14.
@@ -8,6 +8,6 @@
 
 #import <UIKit/UIKit.h>
 
-@interface ViewController : UIViewController
+@interface NearbyViewController : UIViewController
 
 @end
